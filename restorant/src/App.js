9ddbdae0fae6122e902607fe48f32c0aedb87286
MonesAlWissi <alwissi.mones@gmail.com>
2025-02-04 22:33:56 +0300

    .

--- restorant/src/App.js
+++ restorant/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import DrawerAppBar from './nav';
-import HOME from './home';
-import RestaurantFooter from './footer';
+import {Routes,Route} from 'react-router-dom'
 import { Link } from 'react-router-dom';
+import { styled } from '@mui/material';
+import PrimarySearchAppBar from './nav';
 import Menu from './menu';
+import RestaurantFooter from './footer'
+import DrawerAppBar from './nav';
+import Home from './home';
+import React, { useRef } from 'react';
+
+
 function App() {
+
   return (
     
-     <>
+    <Routes>
+    <div className="App">
+    <DrawerAppBar/>
+      
+    {/* <Route path='menu' element={<Menu/>} /> */}
+    <Home></Home>
     
-           <DrawerAppBar></DrawerAppBar>
-           <HOME></HOME>
-           <Menu></Menu>
-            <RestaurantFooter></RestaurantFooter>  
-     </>
+      
+  
      
+    <RestaurantFooter/>
+    
+    </div>
+    
+    </Routes>
     
   );
+ 
 }
 
 export default App;
@@ -27,5 +42,69 @@ export default App;
 
 
 
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import DrawerAppBar from './nav';
+// import HOME from './home';
+// import RestaurantFooter from './footer';
+// import { Link } from 'react-router-dom';
+// import Menu from './menu';
+// import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+// import { styled } from '@mui/material';
+// import React, { useRef } from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import PrimarySearchAppBar from './nav';
+// function App() {
+//   return (
+//     <Router>
+//     <div className="App">
+//      <PrimarySearchAppBar></PrimarySearchAppBar>
+//      {/* <Table></Table> */}
+//       <Routes>
+//       {/* <Route path='/' element={<Home/>}/> */}
+//       <Route path='/Menu' element={<Menu/>}/>
+//       {/* <Route path='/table' element={<TableNew/>}/>
+//       <Route path='/tableTask' element={<TableTask/>}/>
+//       <Route path='/tableTask2' element={<TableTask2/>}/>
+//       <Route path='/Card' element={<Card/>}/>
+//       <Route path='/Timer' element={<Timer/>}/> */}
+//       </Routes>
+//     {/* <DataTable></DataTable>
+//     <TableTest></TableTest>
+//     <DataTablee></DataTablee>
+//     <TableNew></TableNew>
+//     <DataTableee></DataTableee> */}
+//     <RestaurantFooter></RestaurantFooter>  
+//     </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+// function App() {
+//   return (
+
+
+//     <div className='App'>
+//     <PrimarySearchAppBar></PrimarySearchAppBar>
+//            {/* <DrawerAppBar></DrawerAppBar> */}
+  
+//            <HOME></HOME>
+//            <Menu></Menu>
+           
+     
+//             <RestaurantFooter></RestaurantFooter>  
+//             </div>
+     
+          
+//   );
+// }
+
+// export default App;
 
 
